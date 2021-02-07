5cc1ebee2da5ca86c094435e0ac70eff75ec9084
Dibyajyoti Parida <dibyajyoti@centroxy.com>
2021-02-07 13:04:34 +0530

    dbconfig removed confidential

--- dbconfig.js
+++ dbconfig.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // url: 'mongodb://localhost:27017/realtimeData'
-    url: "mongodb+srv://djparida:Dhiramani@6@cluster0.jcpma.mongodb.net/realtimeData?retryWrites=true&w=majority"
+   url:"<your db url>"
 }
